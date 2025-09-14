e8723455266504f65a1493afc4292c6567715684
dmittrey <dmitryzubakhin@mail.ru>
2025-09-14 17:47:44 +0300

    Complete fresh

--- src/Stella/TypeCheck/Unification.hs
+++ src/Stella/TypeCheck/Unification.hs
@@ -2,6 +2,8 @@ module Stella.TypeCheck.Unification where
 
 import Stella.Abs
 
+import Data.Traversable (mapAccumL)
+
 newtype Subs = Subs [(StellaIdent, Unif)] deriving (Eq, Ord, Read)
 
 instance Show Subs where
@@ -10,50 +12,56 @@ instance Show Subs where
       where
         showPair (StellaIdent name, u) = name ++ " = " ++ show u
 
-newtype UnifEq = UnifEq (Unif, Unif) deriving (Eq, Ord, Show, Read)
+unify :: StellaIdent -> Type -> Subs
+unify ident ty = Subs [(ident, UType ty)]
+
+data Unif
+    = UType    Type
+    | UVar     StellaIdent
+    | UArrow   Unif Unif
+  deriving (Eq, Ord, Read)
+
+instance Show Unif where
+    show (UType ty) = show ty
+    show (UVar (StellaIdent name)) = name
+    show (UArrow u1 u2) = "(" ++ show u1 ++ " -> " ++ show u2 ++ ")"
 
 data UnifErr
     = ERROR_OCCURS_CHECK_INFINITE_TYPE
     | ERROR_UNIF_NOT_EQUALS_TYPES Type Type
     | ERROR_UNIF_FAIL
-    | DEBUG StellaIdent Unif
   deriving (Eq, Ord, Show, Read)
 
-data Unif
-    = UnifType    Type
-    | UnifVar     StellaIdent
-    | UnifArrow   Unif Unif
-  deriving (Eq, Ord, Read)
-
-instance Show Unif where
-    show (UnifType ty) = show ty
-    show (UnifVar (StellaIdent name)) = name
-    show (UnifArrow u1 u2) = "(" ++ show u1 ++ " -> " ++ show u2 ++ ")"
-
+newtype UnifEq = UEq (Unif, Unif) deriving (Eq, Ord, Show, Read)
 
 inFreeVars :: StellaIdent -> Unif -> Bool
-inFreeVars ident (UnifType _) =
+inFreeVars ident (UType _) =
     False
-inFreeVars ident (UnifVar varIdent) =
+inFreeVars ident (UVar varIdent) =
     ident == varIdent
-inFreeVars ident (UnifArrow lUnif rUnif) =
-    inFreeVars ident lUnif || inFreeVars ident rUnif
-
+inFreeVars ident (UArrow lu ru) =
+    inFreeVars ident lu || inFreeVars ident ru
 
 subsUnif :: Unif -> -- In which term
             StellaIdent -> -- What we subs
             Unif -> -- On what we subs
             Unif -- Result
 -- No changes
-subsUnif pre@(UnifType _) ident replaceUnif = pre
+subsUnif pre@(UType _) ident replaceUnif = pre
 -- If varIdent match => we subs
-subsUnif pre@(UnifVar varIdent) ident replaceUnif
+subsUnif pre@(UVar varIdent) ident replaceUnif
     | varIdent == ident = replaceUnif
     | otherwise         = pre
 -- Recursive collect replaces UnifArrow
-subsUnif (UnifArrow lUnif rUnif) ident replaceUnif =
-    (UnifArrow (subsUnif lUnif ident replaceUnif) (subsUnif rUnif ident replaceUnif))
+subsUnif (UArrow lu ru) ident replaceUnif =
+    (UArrow (subsUnif lu ident replaceUnif) (subsUnif ru ident replaceUnif))
 
+subsEq :: StellaIdent -> -- Which var we subs
+            Unif -> -- On what we subs
+            UnifEq -> 
+            UnifEq
+subsEq ident replaceUnif (UEq (lu, ru)) =
+    UEq ((subsUnif lu ident replaceUnif), (subsUnif ru ident replaceUnif))
 
 unifSolve :: [UnifEq] -> Either UnifErr Subs
 -- C is emptyList => []
@@ -61,45 +69,86 @@ unifSolve [] = do
     return (Subs [])
 
 -- S = T => unify C'
-unifSolve (UnifEq ((UnifType sTy), (UnifType tTy)) : xs)
+unifSolve (UEq ((UType sTy), (UType tTy)) : xs)
     | sTy /= tTy = Left (ERROR_UNIF_NOT_EQUALS_TYPES sTy tTy)
     | otherwise  = unifSolve xs
 
 -- trivial: same variable on both sides X = X
-unifSolve (UnifEq (UnifVar x, UnifVar y) : xs)
+unifSolve (UEq (UVar x, UVar y) : xs)
     | x == y    = unifSolve xs
 
 -- S is UnifVar X && if X not in FV(T) => unify([X -> T]C') ∘ [X -> T]
-unifSolve (UnifEq ((UnifVar x), t) : xs)
+unifSolve (UEq ((UVar x), t) : xs)
     | inFreeVars x t  = Left ERROR_OCCURS_CHECK_INFINITE_TYPE
     | otherwise       = do
         (Subs unifToType) <- unifSolve (map (subsEq x t) xs)
         return (Subs (unifToType ++ [(x, t)]))
-  where
-    subsEq :: StellaIdent -> -- Which var we subs
-            Unif -> -- On what we subs
-            UnifEq -> 
-            UnifEq
-    subsEq ident replaceUnif (UnifEq (lUnif, rUnif)) =
-        UnifEq ((subsUnif lUnif ident replaceUnif), (subsUnif rUnif ident replaceUnif))
 
 -- T is UnifVar X && if X not in FV(S) => unify([X -> S]C') ∘ [X -> S]
-unifSolve (UnifEq (s, (UnifVar x)) : xs)
+unifSolve (UEq (s, (UVar x)) : xs)
     | inFreeVars x s  = Left ERROR_OCCURS_CHECK_INFINITE_TYPE
     | otherwise       = do
         (Subs unifToType) <- unifSolve (map (subsEq x s) xs)
         return (Subs (unifToType ++ [(x, s)]))
-  where
-    subsEq :: StellaIdent -> -- Which var we subs
-            Unif -> -- On what we subs
-            UnifEq -> 
-            UnifEq
-    subsEq ident replaceUnif (UnifEq (lUnif, rUnif)) =
-        UnifEq ((subsUnif lUnif ident replaceUnif), (subsUnif rUnif ident replaceUnif))
 
 -- S is UnifArrow S1 -> S2 && T is UnifArrow T1 -> T2 => unify(C' ∪ {S1 = T1} ∪ {S2 = T2})
-unifSolve (UnifEq ((UnifArrow s1 s2), (UnifArrow t1 t2)) : xs) =
-    unifSolve (xs ++ [UnifEq (s1, t1), UnifEq (s2, t2)])
+unifSolve (UEq ((UArrow s1 s2), (UArrow t1 t2)) : xs) =
+    unifSolve (xs ++ [UEq (s1, t1), UEq (s2, t2)])
 
 -- else FAIL
 unifSolve _ = Left ERROR_UNIF_FAIL
+
+type LastBusyIdx = Integer
+
+freshVar :: LastBusyIdx -> (LastBusyIdx, Unif)
+freshVar lastIdx =
+    let idx = lastIdx + 1
+        newVar = UVar (StellaIdent ("T" ++ show idx))
+    in (idx, newVar)
+
+freshTy :: LastBusyIdx -> Type -> (LastBusyIdx, Unif)
+freshTy lastIdx TypeAuto        = freshVar lastIdx
+freshTy lastIdx (TypeVar ident) = (lastIdx, UVar ident) -- Not generate new name if ident is exist
+freshTy lastIdx ty              = (lastIdx, UType ty)
+
+data UProgram = UProgram LanguageDecl [Extension] [UDecl]
+  deriving (Eq, Ord, Show, Read)
+
+freshProgram :: LastBusyIdx -> Program -> UProgram
+freshProgram lastIdx (AProgram a1 a2 decls) =
+    let (_, uDecls) = mapAccumL freshDecl lastIdx decls
+    in UProgram a1 a2 uDecls
+
+data UDecl
+    = UDeclFun [Annotation] StellaIdent [UParamDecl] UReturnType ThrowType [UDecl] Expr
+    -- TODO
+    -- | UDeclFunGeneric [Annotation] StellaIdent [StellaIdent] [UParamDecl] UReturnType ThrowType [UDecl] Expr
+    -- | UDeclTypeAlias StellaIdent Type
+    | UDeclExceptionType Type                -- No need to reconstruct types for exceptions
+    | UDeclExceptionVariant StellaIdent Type -- Also
+    deriving (Eq, Ord, Show, Read)
+
+freshDecl :: LastBusyIdx -> Decl -> (LastBusyIdx, UDecl)
+freshDecl lastIdx (DeclFun anns funIdent params retTy throwTy decls expr) =
+    let (newLastIdx,  uDecls) = mapAccumL freshDecl       lastIdx  decls
+        (newLastIdx1, uParams) = mapAccumL freshParamDecl newLastIdx params
+        (newLastIdx2, uRetTy)  = freshReturnType newLastIdx1 retTy
+    in 
+        (newLastIdx2, UDeclFun anns funIdent uParams uRetTy throwTy uDecls expr)
+
+data UParamDecl = UParamDecl StellaIdent Unif
+  deriving (Eq, Ord, Show, Read)
+
+freshParamDecl :: LastBusyIdx -> ParamDecl -> (LastBusyIdx, UParamDecl)
+freshParamDecl lastIdx (AParamDecl ident ty) =
+    let (newLastIdx, unifTy) = freshTy lastIdx ty
+    in (newLastIdx, UParamDecl ident unifTy)
+
+data UReturnType = UNoReturnType | USomeReturnType Unif
+    deriving (Eq, Ord, Show, Read)
+
+freshReturnType :: LastBusyIdx -> ReturnType -> (LastBusyIdx, UReturnType)
+freshReturnType lastIdx NoReturnType = (lastIdx, UNoReturnType)
+freshReturnType lastIdx (SomeReturnType ty) =
+    let (newLastIdx, unifTy) = freshTy lastIdx ty
+    in (newLastIdx, USomeReturnType unifTy)
